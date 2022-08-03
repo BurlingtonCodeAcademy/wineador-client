b907e55fb3806482312e258989909d5c4cf7d560
Paul Niemczyk <mrpndev@gmail.com>
2022-08-03 09:56:51 -0400

    Wineador auth frontend

---
+++ src/components/Auth/Auth.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from "react"
+
+export default function Auth(props) {
+
+    const [ name, setName ] = useState("")
+    const [ email, setEmail ] = useState("")
+    const [ username, setUsername ] = useState("")
+    const [ password, setPassword ] = useState("")
+    const [ login, setLogin ] = useState(true)
+
+    const isLogin = () => {
+        return !login ? "Signup" : "Login"
+    }
+
+    const isSignup = () => {
+        return login ? "Signup": "Login"
+    }
+
+    const toggleLogin = (e) => {
+        e.preventDefault()
+        setLogin(!login)
+        setName("")
+        setEmail("")
+        setUsername("")
+        setPassword("")
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        let requestBody = login ? {
+            username: username,
+            password: password
+        } : {
+            name: name,
+            email: email,
+            username: username,
+            password: password
+        }
+
+        
+    }
+
+    const signup = () => !login ? (
+        <>
+            <div>
+                <label htmlFor="name">Name</label>
+                <input
+                    type="text"
+                    id="name"
+                    value={name}
+                    onChange={e => setName(e.target.value)} />
+            </div>
+            <div>
+                <label htmlFor="email">Email</label>
+                <input
+                    type="email"
+                    id="email"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)} />
+            </div>
+        </>
+    ) : null
+
+    return (
+        <div>
+            <h1>{isLogin()}</h1>
+            <form className="formWrapper" action="">
+                {signup()}
+                <div className="username">
+                <label htmlFor="username">Username:</label>
+                <input
+                    type="text"
+                    id="username"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                />
+                </div>
+                
+                <div className="password">
+                <label htmlFor="password">Password:</label>
+                <input
+                    type="password"
+                    id="password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                />
+                </div>
+                <button onClick={toggleLogin}>{isSignup()}</button>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
+
+
+}
